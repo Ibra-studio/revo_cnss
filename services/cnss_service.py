61e27473bc4c0cf7a14cce4ce3ec8213a1d4a1e9
UIbrahim <mamadouibrahim12345@icloud.com>
2026-05-20 01:29:33 +0100

    ADD: cnss_service

---
+++ services/cnss_service.py
@@ -0,0 +1,40 @@
+from database.connection import get_connection
+from services.employee_service import get_tous_les_employes
+from datetime import datetime
+
+TAUX_SALARIAL=0.06
+TAUX_PATRONAL=0.19
+
+def calculer_cotisation_salarial(salaire_brut):
+    return salaire_brut*TAUX_SALARIAL
+def calculer_cotisation_patronal(salaire_brut):
+    return salaire_brut*TAUX_PATRONAL
+def creer_declaration(mois,annee):
+    conn=get_connection()
+    cursor=conn.cursor()
+
+    try:
+     cursor.execute(
+        "INSERT INTO declarations_cnss(mois,annee,date_generation) VALUES(?,?,?)",
+        (mois,annee,str(datetime.now()))
+    )
+     declarationId=cursor.lastrowid
+
+     employes=get_tous_les_employes()
+
+    
+
+     for e in employes:
+       cursor.execute(
+        "INSERT INTO lignes_declaration(declaration_id,employe_id,salaire_mois,cotisation_salariale,cotisation_patronale) VALUES(?,?,?,?,?)",
+        (declarationId,e["id"],e["salaire_brut"],calculer_cotisation_salarial(e["salaire_brut"]),calculer_cotisation_patronal(e["salaire_brut"]))
+    )
+    
+     conn.commit()
+     return True
+    except Exception as e:
+       conn.rollback()
+       print(f"Erreur: {e}")
+       return False
+
+
